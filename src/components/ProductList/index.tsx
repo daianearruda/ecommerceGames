2e1b49ef9da940eb7f362b1bb46fe6799afdb060
Daiane Arruda <daianedasilvaarruda@outlook.com>
2024-06-03 15:15:44 -0300

    finalizando componentes

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -2,20 +2,51 @@ import Product from '../Product'
 
 import { Container, List } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          infos={['-10%', 'R$ 150']}
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          system="Windows"
+          title="Nome do jogo"
+        />
+
+        <Product
+          infos={['-10%', 'R$ 150']}
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          system="Windows"
+          title="Nome do jogo"
+        />
+
+        <Product
+          infos={['-10%', 'R$ 150']}
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          system="Windows"
+          title="Nome do jogo"
+        />
+
+        <Product
+          infos={['-10%', 'R$ 150']}
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          system="Windows"
+          title="Nome do jogo"
+        />
       </List>
     </div>
   </Container>
